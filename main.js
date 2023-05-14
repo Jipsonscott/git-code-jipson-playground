32dfd4ef3dea8e2804c584fa1293b555892ce8ea
Olaleye Ajibola <olalyeajibola048@gmail.com>
2023-05-14 19:35:36 +0100

    i add all that is needed

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
-console.log("js loaded")
-console.log("good")
+const button = document.querySelector(".button")
+const bigCrad = document.querySelector(".big-card")
+const times = document.querySelector(".times")
+
+
+button.addEventListener("click", () => {
+    bigCrad.classList.toggle("main")
+})
